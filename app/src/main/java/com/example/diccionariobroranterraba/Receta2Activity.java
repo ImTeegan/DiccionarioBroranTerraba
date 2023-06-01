488d352a7828784292ecc2422d760fd1dd935c3a
ImTeegan <jeanqm91@gmail.com>
2023-05-31 19:06:10 -0600

    Activities recetario listos empty

--- app/src/main/java/com/example/diccionariobroranterraba/Receta2Activity.java
+++ app/src/main/java/com/example/diccionariobroranterraba/Receta2Activity.java
@@ -1,14 +1,100 @@
 package com.example.diccionariobroranterraba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Receta2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receta2Activity extends AppCompatActivity implements RecyclerAdapter.itemClickListener{
+
+    private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    ArrayList<ImageAndSoundModel> imageAndSoundModelArrayList;
+
+    private final AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener
+            = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            }
+
+        }
+    };
+
+    private final MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_receta2);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_receta2);
+
+        RecyclerView recyclerView = findViewById(R.id.receta2Recycler);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //Creates an array of images and soundEffects
+        imageAndSoundModelArrayList = new ArrayList<>();
+
+        imageAndSoundModelArrayList.add(new ImageAndSoundModel(R.drawable.adona, R.raw.suspenso, "Dona"));
+        imageAndSoundModelArrayList.add(new ImageAndSoundModel(R.drawable.agatito, R.raw.taantaantan, "Gatito"));
+        imageAndSoundModelArrayList.add(new ImageAndSoundModel(R.drawable.animales, R.raw.trompeta, "Animales"));
+
+        RecyclerAdapter adapter = new RecyclerAdapter(imageAndSoundModelArrayList, getApplicationContext(), this);
+        recyclerView.setLayoutManager(new GridLayoutManager(Receta2Activity.this, 2));
+        recyclerView.setAdapter(adapter);
+    }
+
+    protected void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
+
+    @Override
+    public void itemClick(int position) {
+        releaseMediaPlayer();
+
+        ImageAndSoundModel imageAndSoundModel = imageAndSoundModelArrayList.get(position);
+
+        int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+        if (result == mAudioManager.AUDIOFOCUS_REQUEST_GRANTED){
+            mMediaPlayer = MediaPlayer.create(Receta2Activity.this, imageAndSoundModel.getmAudioResourceID());
+        }
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
     }
 }
